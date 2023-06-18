9d9ba9bb3e0a5051d81da6d6df01dc9364415431
tarthim <pepijnboessenkool@gmail.com>
2023-06-18 18:03:19 +0200

    Indent fixes

--- finished/michelle.js
+++ finished/michelle.js
@@ -16,183 +16,182 @@ const fixedPositions = [
 
 // Generate a boulder div
 const createBoulder = () => {
-    // Create a new div element
-    const boulder = document.createElement('div');
-  
-    // Set the class name for styling
-    boulder.className = 'boulder';
-  
-    // Set the dimensions of the boulder
-    const width = getRandomNumber(10, 30); // Adjust the range as needed
-    const height = getRandomNumber(20, 40); // Adjust the range as needed
-    boulder.style.width = width + 'px';
-    boulder.style.height = height + 'px';
-  
-    // Generate a random border-radius for a non-uniform shape
-    const borderRadius = getRandomNumber(10, 40); // Adjust the range as needed
-    boulder.style.borderRadius = borderRadius + '%';
-  
-    // Set a background color or image to represent the boulder
-    boulder.style.backgroundColor = getRandomColor(); // Adjust the color as needed
-  
-    // Return the created boulder element
-    return boulder;
+  // Create a new div element
+  const boulder = document.createElement('div');
+
+  // Set the class name for styling
+  boulder.className = 'boulder';
+
+  // Set the dimensions of the boulder
+  const width = getRandomNumber(10, 30); // Adjust the range as needed
+  const height = getRandomNumber(20, 40); // Adjust the range as needed
+  boulder.style.width = width + 'px';
+  boulder.style.height = height + 'px';
+
+  // Generate a random border-radius for a non-uniform shape
+  const borderRadius = getRandomNumber(10, 40); // Adjust the range as needed
+  boulder.style.borderRadius = borderRadius + '%';
+
+  // Set a background color or image to represent the boulder
+  boulder.style.backgroundColor = getRandomColor(); // Adjust the color as needed
+
+  // Return the created boulder element
+  return boulder;
 }
 
 // Helper function to generate a random number within a given range
 const getRandomNumber = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 // Random rgb color generator
 const getRandomColor = () => {
-    // Generate random values for red, green, and blue channels
-    const red = Math.floor(Math.random() * 256);
-    const green = Math.floor(Math.random() * 256);
-    const blue = Math.floor(Math.random() * 256);
-  
-    // Create the CSS color string in RGB format
-    const color = `rgb(${red}, ${green}, ${blue})`;
-  
-    // Return the random color
-    return color;
+  // Generate random values for red, green, and blue channels
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  // Create the CSS color string in RGB format
+  const color = `rgb(${red}, ${green}, ${blue})`;
+
+  // Return the random color
+  return color;
 }
 
 // Generate random boulder wall with boulders of random sizes and colors
 const initBoulderWall = () => {
-    // Get the initial dimensions of the boulder wall
-    const wallWidth = boulderWall.offsetWidth;
-    const wallHeight = boulderWall.offsetHeight;
+  // Get the initial dimensions of the boulder wall
+  const wallWidth = boulderWall.offsetWidth;
+  const wallHeight = boulderWall.offsetHeight;
 
-    const boulderCount = 45;
-    for (let i = 0; i < boulderCount; i++) {
-        let b = createBoulder();
-        boulderWall.appendChild(b);
-        
-        // Calculate the scaling factor for the boulder
-        const scaleX = wallWidth / boulderWall.offsetWidth;
-        const scaleY = wallHeight / boulderWall.offsetHeight;
+  const boulderCount = 45;
+  for (let i = 0; i < boulderCount; i++) {
+      let b = createBoulder();
+      boulderWall.appendChild(b);
+      
+      // Calculate the scaling factor for the boulder
+      const scaleX = wallWidth / boulderWall.offsetWidth;
+      const scaleY = wallHeight / boulderWall.offsetHeight;
 
-        // Move boulder to random coordinates inside the boulder wall
-        let l = getRandomNumber(0, boulderWall.offsetWidth - b.offsetWidth);
-        let t = getRandomNumber(0, boulderWall.offsetHeight - b.offsetHeight);
+      // Move boulder to random coordinates inside the boulder wall
+      let l = getRandomNumber(0, boulderWall.offsetWidth - b.offsetWidth);
+      let t = getRandomNumber(0, boulderWall.offsetHeight - b.offsetHeight);
 
-        // Apply scaling and positioning to the boulder
-        b.style.transformOrigin = 'top left';
-        b.style.transform = `scale(${scaleX}, ${scaleY}) translate(${l}px, ${t}px)`;
-        b.style.position = 'absolute';
-    }
+      // Apply scaling and positioning to the boulder
+      b.style.transformOrigin = 'top left';
+      b.style.transform = `scale(${scaleX}, ${scaleY}) translate(${l}px, ${t}px)`;
+      b.style.position = 'absolute';
+  }
 }
 
 // Generate fixed boulders
 function positionFixedItems() {
-    const boulderWall = document.getElementById('boulder-wall');
+  const boulderWall = document.getElementById('boulder-wall');
 
-    fixedPositions.forEach((position) => {
-      const boulder = document.createElement('div');
-      boulder.className = 'boulder-fixed';
-      boulder.style.left = position.left + '%';
-      boulder.style.top = position.top + '%';
+  fixedPositions.forEach((position) => {
+    const boulder = document.createElement('div');
+    boulder.className = 'boulder-fixed';
+    boulder.style.left = position.left + '%';
+    boulder.style.top = position.top + '%';
 
-      const size = getRandomNumber(20, 40);
-      boulder.style.width = size + 'px';
-      boulder.style.height = size + 'px';
-  
-      boulderWall.appendChild(boulder);
-    });
+    const size = getRandomNumber(20, 40);
+    boulder.style.width = size + 'px';
+    boulder.style.height = size + 'px';
+
+    boulderWall.appendChild(boulder);
+  });
 }
   
 
 // Generate michelle moving around
 
 class MovableObject {
-    constructor() {
-      this.element = document.getElementById('moving-object');
-      this.currentIndex = 0;
-      this.fixedPositions = fixedPositions;
-      this.animationInterval = null;
+  constructor() {
+    this.element = document.getElementById('moving-object');
+    this.currentIndex = 0;
+    this.fixedPositions = fixedPositions;
+    this.animationInterval = null;
+  }
+
+  move() {
+    const currentPosition = this.fixedPositions[this.currentIndex];
+    this.element.style.left = currentPosition.left + '%';
+    this.element.style.top = currentPosition.top + '%';
+
+    this.currentIndex++;
+    if (this.currentIndex === this.fixedPositions.length) {
+      // Clear interval
+      clearInterval(this.animationInterval);
+      this.finishAnimation();
     }
-  
-    move() {
-      const currentPosition = this.fixedPositions[this.currentIndex];
-      this.element.style.left = currentPosition.left + '%';
-      this.element.style.top = currentPosition.top + '%';
-  
-      this.currentIndex++;
-      if (this.currentIndex === this.fixedPositions.length) {
-        // Clear interval
-        clearInterval(this.animationInterval);
-        this.finishAnimation();
-      }
-      else {
-        if (this.currentIndex === 6) {
-          this.replaceImage();
-        }
+    else {
+      if (this.currentIndex === 6) {
+        this.replaceImage();
       }
     }
+  }
+
+  replaceImage() {
+    setTimeout(() => {
+      const newSrc = "./assets/michelle-smile.png";
+
+      // Apply class for transition effect
+      this.element.classList.add('replace-animation');
   
-    replaceImage() {
+      // Triggering the transition
       setTimeout(() => {
-        const newSrc = "./assets/michelle-smile.png";
+        this.element.style.opacity = 0;
+      }, 10);
+  
+      // Updating the image source after the transition
+      setTimeout(() => {
+        this.element.src = newSrc;
+        this.element.style.opacity = 1;
+      }, 150);
+    }, 400);
+  }
 
-        // Apply class for transition effect
-        this.element.classList.add('replace-animation');
-    
-        // Triggering the transition
-        setTimeout(() => {
-          this.element.style.opacity = 0;
-        }, 10);
-    
-        // Updating the image source after the transition
-        setTimeout(() => {
-          this.element.src = newSrc;
-          this.element.style.opacity = 1;
-        }, 150);
-      }, 400);
-    }
+  startAnimation() {
+    this.move();
+    this.animationInterval = setInterval(() => this.move(), 500); // Adjust the interval as needed
+  }
 
-    startAnimation() {
-      this.move();
-      this.animationInterval = setInterval(() => this.move(), 500); // Adjust the interval as needed
-    }
-
-    finishAnimation() {
-        console.log('Animation is finished');
-        animateConfetti();
-        // Small timeout before blur
-        setTimeout(() => {
-            // Show the last screen
-            prepLastScreen();
-        }, 700); // Adjust the duration as needed
-    }
+  finishAnimation() {
+      console.log('Animation is finished');
+      animateConfetti();
+      // Small timeout before blur
+      setTimeout(() => {
+          // Show the last screen
+          prepLastScreen();
+      }, 700); // Adjust the duration as needed
+  }
 }
-
 
 function createConfetti() {
-    const confettiContainer = document.getElementById('michelle-app');
-  
-    // Create a confetti element
-    const confetti = document.createElement('div');
-    confetti.classList.add('confetti');
-  
-    // Set a random position for the confetti element
-    const randomX = Math.random() * window.innerWidth;
-    confetti.style.left = randomX + 'px';
-    confetti.style.top = '-10px';
-  
-    // Add the confetti element to the container
-    confettiContainer.appendChild(confetti);
-  
-    // Remove the confetti element after it falls out of the screen
-    setTimeout(() => {
-      confetti.remove();
-    }, 7000); // Adjust the duration as needed
+  const confettiContainer = document.getElementById('michelle-app');
+
+  // Create a confetti element
+  const confetti = document.createElement('div');
+  confetti.classList.add('confetti');
+
+  // Set a random position for the confetti element
+  const randomX = Math.random() * window.innerWidth;
+  confetti.style.left = randomX + 'px';
+  confetti.style.top = '-10px';
+
+  // Add the confetti element to the container
+  confettiContainer.appendChild(confetti);
+
+  // Remove the confetti element after it falls out of the screen
+  setTimeout(() => {
+    confetti.remove();
+  }, 7000); // Adjust the duration as needed
 }
-  
+
 function animateConfetti() {
-setInterval(() => {
-    createConfetti();
-}, 100); // Adjust the interval as needed
+  setInterval(() => {
+      createConfetti();
+  }, 100); // Adjust the interval as needed
 }
 
 // Generate random boulder wall
@@ -203,7 +202,7 @@ positionFixedItems();
 const movableObject = new MovableObject();
 movableObject.startAnimation();
 
-
+// Called through movable object when it is done moving
 const prepLastScreen = () => {
   // Blur out the boulder wall
   boulderWall.style.filter = 'brightness(0.8) blur(5px)';
